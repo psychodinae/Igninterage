e1d3b6b41d6bca0c7050893777b53a966246af57
psychodinae <67171012+psychodinae@users.noreply.github.com>
2020-06-23 13:23:31 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,12 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-
-def main():
-    import igninterage as app
-    setuptools.setup(
-        name=app.__project__,
-        version=app.__version__,
-        author=app.__author__,
-        author_email=app.__author_email__,
-        description=app.__description__,
-        long_description=long_description,
-        license=app.__licence__,
-        packages=['igninterage'],
-        classifiers=app.__classifiers__,
-        python_requires=app.__python_requires__,
-        install_requires=['requests', 'selenium'],
-    )
-
-
-if __name__ == '__main__':
-    main()
+setup(name='igninterage',
+      version='0.1',
+      description='Modulo para interagir no forum IGNboards',
+      url='https://github.com/psychodinae',
+      author='Psychodinae',
+      author_email='https://github.com/psychodinae',
+      license='MIT',
+      packages=['funniest'],
+      install_requires=['requests', 'selenium']
+     )
