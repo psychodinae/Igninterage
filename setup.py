97b18763d423a93acbf531833a2d6f596c750526
psychodinae <67171012+psychodinae@users.noreply.github.com>
2020-07-20 14:30:17 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='igninterage',
-      version='0.1',
+      version='1.0',
       description='Modulo para interagir no forum IGNboards',
       url='https://github.com/psychodinae',
       author='Psychodinae',
       author_email='https://github.com/psychodinae',
       license='MIT',
       packages=['igninterage'],
-      install_requires=['requests', 'selenium'],
+      install_requires=['requests', 'lxml'],
       python_requires='>=3.6'
-     )
+      )
