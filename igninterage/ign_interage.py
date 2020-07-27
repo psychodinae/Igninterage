0ed65adf76497da51c5fbd9b94fe2f0cffeec3cb
mellon_collie <67171012+psychodinae@users.noreply.github.com>
2020-07-27 15:45:00 -0300

    Update ign_interage.py
    Add cache para Xenforo default login e enviar mensagem privada.

--- igninterage/ign_interage.py
+++ igninterage/ign_interage.py
@@ -5,7 +5,7 @@ from igninterage.interage import Interage
 
 
 class Igninterage(Interage):
-    def __init__(self, cache_file_name, url: str, header=Interage.headers):
+    def __init__(self, cache_file_name, url, header=Interage.headers):
         """Clase principal do modulo. Responsavel por recuperar os cookies do navegador
         Firefox, salva-los em arquivo de cache e utiliza-los para realizar as requisições,
         no fórum IGN Boards.
@@ -14,17 +14,15 @@ class Igninterage(Interage):
             * Postar em um tpc.
             * Editar o post.
             * reagir a um post.
-        TODO:
             * Enviar mensagem privada.
-            
+
         Args:
             cache_file_name (str): caminho/nome para salvar o arquivo de cache com o cookie de login.
             url (str): url do forum.
             header : Opcional, para inserir um User-agent customizado.
         """
-        super().__init__(url, header)
+        super(Igninterage, self).__init__(url, header)
         self._cache_file_name = cache_file_name
-
 
     def _load_cache_cookies(self):
         try:
@@ -41,11 +39,17 @@ class Igninterage(Interage):
         except (ConnectionError, ex.NotXenforoPage, ex.LoginError):
             raise
         utils.save_cookie_file(cookie, self._cache_file_name)
-        print('[!] O arquivo cache da sessão está expirado, criando um novo usando o navegador firefox...')
+        print('[!] O arquivo cache da sessão está nao existe ou esta expirado, criando um novo usando o navegador '
+              'firefox...')
         return True
+
+    def xenforo2_login(self, username, password):
+        if not self._load_cache_cookies():
+            ck = self._xenforo2_login(username, password)
+            utils.save_cookie_file(ck, self._cache_file_name)
+        print(f'[!] Cookies carregados do arquivo "{self._cache_file_name}"')
 
     def ign_login(self):
         if not self._load_cache_cookies():
             self._load_from_firefox()
         print(f'[!] Cookies carregados do arquivo "{self._cache_file_name}"')
-
