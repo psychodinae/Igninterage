99ccae82f38225deda7a1262ad4dc8baaf091ba9
mellon_collie <67171012+psychodinae@users.noreply.github.com>
2020-07-27 16:02:21 -0300

    Update interage.py

--- igninterage/interage.py
+++ igninterage/interage.py
@@ -14,7 +14,7 @@ def decorator_check_login(f):
 
 
 class Interage(object):
-    """Clase que realiza as requisições web"""
+    """Classe que realiza as requisições web"""
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/70.0.3538.77 Safari/537.36'
