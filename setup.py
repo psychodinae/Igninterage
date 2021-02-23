f08188a0e9b780d33d6c8a6ca2bed30814697b45
mellon_collie <noteprof213@gmail.com>
2021-02-23 18:53:14 -0300

    update versao 1.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 
 setup(name='igninterage',
-      version='1.2.0',
+      version='1.2.1',
       description='Modulo para interagir no forum IGNboards+',
       long_description=long_description,
       long_description_content_type="text/markdown",
